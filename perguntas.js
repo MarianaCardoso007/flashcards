98c53dd148fc29ce88aad1eb0c22f446e347cfc9
MarianaCardoso007 <00001105305569SP@al.educacao.sp.gov.br>
2024-12-06 12:40:13 -0300

    primeira versão

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dan Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "Bom dia" em alemão?',
+    'Bom dia em alemão é "Guten Morgen"'
+)
